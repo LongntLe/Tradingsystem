3f312abfa75d6fb8b70f56d0fccc35ea079ef539
LongntLe <long@minerva.kgi.edu>
2017-05-09 11:40:48 -0700

    reorganize modules so relations are clearer. For now some modules are not put into files as they are subject to constant changes in the time being

---
+++ src/price_handler/db_price_handler.py
@@ -0,0 +1,26 @@
+import PriceHandler
+import price_retrieval
+import os.path
+import pandas as pd
+
+
+class DBPriceHandler(PriceHandler):
+    def __init__(self, instrument, start_date, end_date, events_queue, db_dir):
+        self.instrument = instrument
+        self.start_date = start_date
+        self.end_date = end_date
+        self.events_queue = events_queue
+        self.db_dir = db_dir
+        self.cur_bid = None
+        self.cur_ask = None
+
+    def _retrieve_prices(self):
+        if os.path.isfile("%s.h5" % self.instrument) == "False":
+            price_retrieval.get_data(self.instrument, self.start_date, self.end_date)
+
+        else:
+            pd.read_hdf("%s.h5" % self.instrument)
+
+    def stream_to_queue(self):
+        self.retrieve_prices()
+        for index, row in self
---
+++ src/streaming.py
@@ -0,0 +1,55 @@
+from event import TickEvent
+import v20
+import pandas as pd
+
+
+class StreamingForexPrices(object):
+    def __init__(
+            self, domain, access_token,
+            account_id, instruments, events_queue
+    ):
+        # self.ticks = 0
+        # self.data = pd.DataFrame()
+        self.domain = domain
+        self.access_token = access_token
+        self.account_id = account_id
+        self.instruments = instruments
+        self.events_queue = events_queue
+        self.cur_bid = None
+        self.cur_ask = None
+        self.ctx_stream = v20.Context(
+            self.domain,
+            443,
+            True,
+            application="sample_code",
+            token=self.access_token,
+            datetime_format="RFC3339"
+        )
+
+    def stream_to_queue(self):
+        response = self.ctx_stream.pricing.stream(
+            self.account_id,
+            snapshot=True,
+            instruments=self.instruments
+        )
+        for msg_type, msg in response.parts():
+            if msg_type == "pricing.Price":
+                # msg = self.on_success(msg.time, msg.asks[0].price)
+                print(msg.instrument, msg.time, msg.asks[0].price, msg.bids[0].price)
+                instrument = msg.instrument
+                time = msg.time
+                bid = msg.bids[0].price
+                ask = msg.asks[0].price
+                tev = TickEvent(instrument, time, bid, ask)
+                self.events_queue.put(tev)
+                self.cur_bid = bid
+                self.cur_ask = ask
+                # self.ticks += 1
+                # print(self.ticks, end=' ')
+                # self.data = self.data.append(
+                #        pd.DataFrame({"time": [time], "ask": [ask]}))
+                # self.data.index = pd.DatetimeIndex(self.data["time"])
+                # resam = self.data.resample("1min").last()
+                # print(resam[["ask", "returns", "positions"]].tail())
+            elif msg_type == "pricing.Heartbeat" and args.show_heartbeats:
+                print(msg)
